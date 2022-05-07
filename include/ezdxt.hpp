ad62cb803e0eea6cd35887a20249d0af54940a91
JTurtle <jturtl@pm.me>
2022-05-07 15:08:58 -0700

    Update C interface

--- include/ezdxt.hpp
+++ include/ezdxt.hpp
@@ -1,115 +1,62 @@
-#ifndef _opensrc_ezdxt_hpp
-#define _opensrc_ezdxt_hpp
+#ifndef _ezdxt_hpp
+#define _ezdxt_hpp
 
 #include <cstdint>
 #include <optional>
 
 namespace ezdxt {
-    struct Rgb565;
-    struct Rgbf {
-        float r, g, b;
-        static Rgbf from_rgb565(Rgb565);
+    struct Color {
+        float r, g, b, a;
     };
-
-    struct Rgb565 {
-        uint16_t data;
-
-        static Rgb565 from_int(uint16_t data) {
-            return Rgb565 {
-                .data = data,
-            };
-        }
-
-        uint8_t r() const {
-            return uint8_t((data & 0xF800) >> 11);
-        }
-        uint8_t g() const {
-            return uint8_t((data & 0x07E0) >> 5);
-        }
-        uint8_t b() const {
-            return uint8_t(data & 0x001F);
-        }
-
-    };
-
-    struct Rgb888 {
-        uint8_t r, g, b;
-
-        Rgb888() : r(0), g(0), b(0) {}
-
-        static Rgb888 from_rgbf(Rgbf rgbf) {
-            Rgb888 me;
-            me.r = (uint8_t)(rgbf.r * 255);
-            me.g = (uint8_t)(rgbf.g * 255);
-            me.b = (uint8_t)(rgbf.b * 255);
-            return me;
-        }
-
-        static Rgb888 from_rgb565(Rgb565 rgb) {
-            const auto rgbf = Rgbf::from_rgb565(rgb);
-            return from_rgbf(rgbf);
-        }
-    };
-
-    // very reasonable place to put this function
-    // for a very reasonable language like C++
-    Rgbf Rgbf::from_rgb565(Rgb565 rgb) {
-        Rgbf me;
-        me.r = (float)rgb.r() / 31;
-        me.g = (float)rgb.g() / 63;
-        me.b = (float)rgb.b() / 31;
-        return me;
-    }
-
 
     struct Image {
-        const void *data;
-        uint16_t width, height;
+        const uint8_t *data;
+        uint16_t width;
+        uint16_t height;
     };
 
-    struct Dxt1Pixel {
-        Rgb565 color;
-        bool has_color;
-    };
 }
 
 extern "C" {
-    ezdxt::Dxt1Pixel ezdxt_get_pixel_dxt1(ezdxt::Image,uint16_t,uint16_t);
-    ezdxt::Dxt1Pixel ezdxt_get_pixel_dxt1_chunk(const uint8_t*,uint8_t,uint8_t);
+    ezdxt::Color ezdxt1_get_pixel(ezdxt::Image, uint16_t x, uint16_t y);
+    ezdxt::Color ezdxt1_get_pixel_chunk(const uint8_t *data, uint8_t x, uint8_t y);
+    ezdxt::Color ezdxt3_get_pixel(ezdxt::Image, uint16_t x, uint16_t y);
+    ezdxt::Color ezdxt3_get_pixel_chunk(const uint8_t *data, uint8_t x, uint8_t y);
 }
 
 namespace ezdxt {
-    // What a journey, the pixel goes from a Zig optional to a C struct to a C++ optional
-    inline std::optional<Rgb565> get_pixel_dxt1(Image image, uint16_t x, uint16_t y) {
-        const Dxt1Pixel px = ezdxt_get_pixel_dxt1(image, x, y);
-        if (px.has_color)
-            return px.color;
-        else
-            return std::nullopt;
+    namespace dxt1 {
+        inline ezdxt::Color get_pixel(ezdxt::Image img, uint16_t x, uint16_t y) {
+            return ezdxt1_get_pixel(img, x, y);
+        }
+        inline ezdxt::Color get_pixel_chunk(const uint8_t *data, uint16_t x, uint16_t y) {
+            return ezdxt1_get_pixel_chunk(data, x, y);
+        }
+        inline ezdxt::Color get_pixel_noalpha(ezdxt::Image img, uint16_t x, uint16_t y) {
+            auto px = get_pixel(img, x, y);
+            if (px.a == 0) {
+                px.a = 1;
+                px.r = px.g = px.b = 0;
+            }
+            return px;
+        }
+        inline ezdxt::Color get_pixel_chunk_noalpha(const uint8_t *data, uint16_t x, uint16_t y) {
+            auto px = get_pixel_chunk(data, x, y);
+            if (px.a == 0) {
+                px.a = 1;
+                px.r = px.g = px.b = 0;
+            }
+            return px;
+        }
     }
 
-    inline std::optional<Rgb565> get_pixel_dxt1_chunk(const uint8_t *data, uint8_t x, uint8_t y) {
-        const Dxt1Pixel px = ezdxt_get_pixel_dxt1_chunk(data, x, y);
-        if (px.has_color)
-            return px.color;
-        else
-            return std::nullopt;
-    }
-
-    inline Rgb565 get_pixel_dxt1_noalpha(Image image, uint16_t x, uint16_t y) {
-        const Dxt1Pixel px = ezdxt_get_pixel_dxt1(image, x, y);
-        if (px.has_color)
-            return px.color;
-        else
-            return Rgb565::from_int(0);
-    }
-
-    inline Rgb565 get_pixel_dxt1_chunk_noalpha(const uint8_t *data, uint8_t x, uint8_t y) {
-        const Dxt1Pixel px = ezdxt_get_pixel_dxt1_chunk(data, x, y);
-        if (px.has_color)
-            return px.color;
-        else
-            return Rgb565::from_int(0);
+    namespace dxt3 {
+        inline ezdxt::Color get_pixel(ezdxt::Image img, uint16_t x, uint16_t y) {
+            return ezdxt3_get_pixel(img, x, y);
+        }
+        inline ezdxt::Color get_pixel_chunk(const uint8_t *data, uint16_t x, uint16_t y) {
+            return ezdxt3_get_pixel_chunk(data, x, y);
+        }
     }
 }
 
--- src/exports.zig
+++ src/exports.zig
@@ -2,21 +2,15 @@ const ezdxt = @import("main.zig");
 const dxt1 = ezdxt.dxt1;
 const dxt3 = ezdxt.dxt3;
 const dxt5 = ezdxt.dxt5;
+const Rgba = ezdxt.Rgba;
 
+//todo: make ezdxt.Image an extern struct?
 const Image = extern struct {
     data: [*]const u8,
     width: u16,
     height: u16,
 
-    pub fn toEzdxt1(self: @This()) ezdxt.Image {
-        // - calculate length of data, assuming DXT1 format -
-        // ezdxt.Image.data being a slice and not a pointer-to-many
-        // is intentional, as it allows runtime safety checks.
-        // This might change because
-        // 1. with safety checks disabled, data.len is not used
-        // 2. extern structs cannot have slices (hence this struct)
-        // The result of this function does not guarantee that data.len covers
-        // the actual amount of memory allocated in data.ptr
+    pub fn toEzdxt(self: @This()) ezdxt.Image {
         var data: []const u8 = undefined;
         data.ptr = self.data;
         data.len = (@as(u32, self.width) * self.height) / 2;
@@ -28,11 +22,6 @@ const Image = extern struct {
     }
 };
 
-/// wrapper for nullable ?ezdxt.Rgb565
-const Dxt1Pixel = extern struct {
-    color: ezdxt.Rgb565,
-    has_color: bool,
-};
 
 //TODO: should `dxt1.getPixel[Chunk]NoAlpha()` be exported, or left for
 // the individual language bindings to recreate, considering their simplicity?
@@ -41,35 +30,29 @@ export fn ezdxt1_get_pixel(
     image: Image,
     x: u16,
     y: u16,
-) Dxt1Pixel {
-    if (ezdxt.dxt1.getPixel(image.toEzdxt1(), x, y)) |px|
-        return .{ .has_color=true, .color=px }
-    else
-        return .{ .has_color=false, .color=undefined };
+) Rgba {
+    return dxt1.getPixel(image.toEzdxt(), x, y);
 }
 
 export fn ezdxt1_get_pixel_chunk(
     data: *const [8]u8,
     x: u8,
     y: u8,
-) Dxt1Pixel {
+) Rgba {
     // @intCast not @truncate, for runtime safety+catching invalid arguments
     const _x = @intCast(u2, x);
     const _y = @intCast(u2, y);
 
-    if (dxt1.getPixelChunk(data, _x, _y)) |px|
-        return .{ .has_color=true, .color=px }
-    else
-        return .{ .has_color=false, .color=undefined };
+    return dxt1.getPixelChunk(data, _x, _y);
 }
 
 export fn ezdxt3_get_pixel(
     image: Image,
     x: u16,
     y: u16,
-) ezdxt.Rgba5654 {
-    return ezdxt.dxt3.getPixel(
-        image.toEzdxt1(),
+) Rgba {
+    return dxt3.getPixel(
+        image.toEzdxt(),
         @intCast(u2, x),
         @intCast(u2, y),
     );
@@ -79,8 +62,8 @@ export fn ezdxt3_get_pixel_chunk(
     data: *const [16]u8,
     x: u8,
     y: u8,
-) ezdxt.Rgba5654 {
-    return ezdxt.dxt3.getPixelChunk(
+) Rgba {
+    return dxt3.getPixelChunk(
         data,
         @intCast(u2, x),
         @intCast(u2, y),
