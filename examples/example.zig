3bed5ebd67d5f20bcf1ff4549f4d9c3b5cece4c7
JTurtle <jturtl@pm.me>
2022-08-06 15:49:47 -0700

    Simplify example

--- examples/example.zig
+++ examples/example.zig
@@ -2,33 +2,35 @@ const std = @import("std");
 const ezdxt = @import("ezdxt");
 const loss = @embedFile("loss_dxt1.bin");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer {
-        _ = gpa.deinit();
-    }
-    const allocator = gpa.allocator();
+const allocator = std.heap.page_allocator;
 
-    const buf = try allocator.alloc(u8, 42069);
-    defer allocator.free(buf);
+pub fn main() !void {
     const img = ezdxt.Image {
         .data = loss,
         .width = 8,
         .height = 8,
     };
 
-    var idx: usize = 0;
-    const print = std.fmt.bufPrint;
-    idx += (try print(buf[idx..], "P3 8 8 255\n", .{})).len;
+    // max 12 bytes per pixel ("NNN NNN NNN\n") times 64 (8x8 pixels)
+    // plus 11 (header, "P3 8 8 255\n")
+    const bufsz = 12 * 8*8 + 11;
+    const buf = try allocator.alloc(u8, bufsz);
+    defer allocator.free(buf);
+
+    var stream = std.io.fixedBufferStream(buf);
+    const writer = stream.writer();
+
+    try writer.writeAll("P3 8 8 255\n");
+
     var y: u4 = 0;
     while (y < 8) : (y += 1) {
         var x: u4 = 0;
         while (x < 8) : (x += 1) {
             const px = ezdxt.dxt1.getPixelNoAlpha(img, x, y);
             const rgb = px.asRgb888();
-            idx += (try print(buf[idx..], "{} {} {}\n", .{rgb.r, rgb.g, rgb.b})).len;
+            try writer.print("{} {} {}\n", .{rgb.r, rgb.g, rgb.b});
         }
     }
 
-    try std.io.getStdOut().writeAll(buf[0..idx]);
+    try std.io.getStdOut().writeAll(buf[0..stream.pos]);
 }
